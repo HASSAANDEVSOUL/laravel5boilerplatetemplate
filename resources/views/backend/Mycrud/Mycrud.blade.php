d5e7d7696457d9dd04984a547ec34df28d794e37
HASSAAN ANWER L1F11BSCS2103 <h_anwer@ucp.edu.pk>
2018-01-10 00:59:09 +0500

    Edit and Delete buttons added and bootstrap applied on them

--- resources/views/backend/Mycrud/Mycrud.blade.php
+++ resources/views/backend/Mycrud/Mycrud.blade.php
@@ -2,28 +2,27 @@
 
 @section('content')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>    <script>
         $(document).ready(function()
         {
             $('#myt').DataTable();});
     </script>
-    <table class="table bordered" id="myt">
+    <table class="table bordered" id="myt" style="width: 50%;">
 
         <thead>
         <th>Id</th>
         <th>Name</th>
+        <th>Actions</th>
         </thead>
         <tbody>
         @foreach($data as $data)
             <tr>
                 <td>{{$data->id}}</td>
                 <td>{{$data->name}}</td>
+                <td><button class="btn btn-danger">Edit</button>
+                    <button type="button" class="btn btn-success">Delete</button></td>
             </tr>
         @endforeach
-        <tr>
-            <td>1</td>
-            <td>2</td>
-        </tr>
 
         </tbody>
     </table>
